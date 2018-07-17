0bfad5365f04e53d175d1a6cd496cc8e11ae36eb
Sergio Barrera <sdbeng@gmail.com>
2018-07-17 14:51:33 -0700

    Fix end block missing

--- users/views.py
+++ users/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     #Do the work here...
-    response = "<h1>Hello, world!</h1>"
+    # response = "<h1>Hello, world!</h1>"
 
     # ... and return a response with its results
-    return HttpResponse(response)
+    # return HttpResponse(response)
+    return render(request, 'users/home.html', {})
 
 def detail(request):
     return HttpResponse("<h1>detail views!!</h1>")
