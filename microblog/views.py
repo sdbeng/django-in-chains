8ff3a317481cb6f5bec47af1ccb0f660abca1268
Sergio Barrera <sdbeng@gmail.com>
2018-07-19 09:19:44 -0700

    Add concept about django views

--- microblog/views.py
+++ microblog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from microblog.models import Tweet
 
-# Create your views here.
+# Create your views here. Remember django views are controllers
 def homepage(request):
     all_tweets = Tweet.objects.all()
     context = {
