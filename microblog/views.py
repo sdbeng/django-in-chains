7f213af92169453daeaed50b16038ade808db139
Sergio Barrera <sdbeng@gmail.com>
2018-07-19 19:47:05 -0700

    Add a textare form input to save post messages

--- microblog/views.py
+++ microblog/views.py
@@ -3,6 +3,11 @@ from microblog.models import Tweet
 
 # Create your views here. Remember django views are controllers
 def homepage(request):
+    if request.method == "POST":
+        Tweet.objects.create(
+            text=request.POST["text"],
+            user=request.user,
+        )
     all_tweets = Tweet.objects.all()
     context = {
         'tweets': all_tweets,
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^home/$', views.home, name='home'),
-    url(r'^detail/(?P<user_id>[0-9]/$)', views.detail, name='detail'),
+    # url(r'^home/$', views.home, name='home'),
+    path('', views.home, name='home'),
+    # url(r'^detail/(?P<user_id>[0-9]/$)', views.detail, name='detail'),
+    path('<int:user_id>/', views.detail, name='detail'),
     # url(r'^detail/$', views.detail, name='detail'),
     url(r'^add/$', views.add, name='add'),
 ]
